9126874312f76db0575c6521bf98f6828ffd874f
ngoka martins <ngokamartins@hotmail.com>
2023-11-30 10:19:08 -0800

    task3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
+def add_arg(argv):
+    v = len(argv) - 1
+    if v == 0:
+        print("{:d}".format(v))
+        return
+    else:
+        i = 1
+        add = 0
+        while i <= v:
+            add += int(argv[i])
+            i += 1
+        print("{:d}".format(add))
+
+
 if __name__ == "__main__":
-    import sys, math
-    result = 0
-    for i in sys.argv:
-        result += int()
-        print("{}".format(result))
+    import sys
+    add_arg(sys.argv)
+
